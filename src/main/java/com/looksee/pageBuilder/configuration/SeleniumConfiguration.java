eaf7b0baac6ce05f9f76e9a8e7fcea6c192aab8d
Brandon Kindred <brandon.kindred@gmail.com>
2025-07-11 01:26:51 -0400

    fix: download core

--- src/main/java/com/looksee/pageBuilder/configuration/SeleniumConfiguration.java
+++ src/main/java/com/looksee/pageBuilder/configuration/SeleniumConfiguration.java
@@ -1,9 +1,11 @@
-package com.looksee.configuration;
+package com.looksee.pageBuilder.configuration;
 
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+
+import com.looksee.browsing.helpers.BrowserConnectionHelper;
 
 /**
  * Configuration class for Selenium settings
